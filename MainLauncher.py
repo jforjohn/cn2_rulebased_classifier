89886ff11a4ccc4bca4ec481b3e29c8c2e16e91b
jforjohn <giannisotiro@gmail.com>
2019-03-31 20:00:32 +0200

    MAIN: add defaults for options except dataset

--- MainLauncher.py
+++ MainLauncher.py
@@ -36,32 +36,38 @@ if __name__ == '__main__':
     try:
         bins_no = config.getint('cn2', 'bins_no')
     except ValueError:
-        print('bins_no should be integer')
-        sys.exit(1)
+        print('bins_no should be integer, default: 5')
+        bins_no = 5
 
     try:
         beam_width = config.getint('cn2', 'beam_width')
     except ValueError:
-        print('beam_width should be integer')
-        sys.exit(1)
+        print('beam_width should be integer, default: 3')
+        beam_width = 3
 
     try:
         min_significance = config.getfloat('cn2', 'min_significance')
     except ValueError:
-        print('min_significance should be float')
-        sys.exit(1)
+        print('min_significance should be float, default: 0.5')
+        min_significance = 0.5
 
     try:
         negate = config.getboolean('cn2', 'negate')
     except ValueError:
-        print('negate should be boolean or yes/no')
-        sys.exit(1)
+        print('negate should be boolean or yes/no, default: no')
+        negate = False
 
     try:
         disjunctive = config.getboolean('cn2', 'disjunctive')
     except ValueError:
-        print('disjunctive should be boolean or yes/no')
-        sys.exit(1)
+        print('disjunctive should be boolean or yes/no, default: no')
+        disjunctive = False
+    
+    try:
+        train_percentage = config.getfloat('cn2', 'train_percentage')
+    except ValueError:
+        print('train_percentage should be float, default: 0.7')
+        train_percentage = 0.7
 
     ## Preprocessing
     preprocess = MyPreprocessing(bins_no=bins_no)
@@ -70,7 +76,7 @@ if __name__ == '__main__':
     labels = preprocess.labels_
 
     x_train, x_test, y_train, y_test = train_test_split(
-        df , labels, train_size=0.7, random_state=42, stratify=labels)
+        df , labels, train_size=train_percentage, random_state=42, stratify=labels)
     print(x_test.head())
     print(y_test.head())
     cn2 = MyCN2(beam_width=beam_width,
